df259857563dc57455f6d2b3014e15cadce06558
renik <mrushvg@gmail.com>
2023-08-26 05:47:17 -0700

    update botania mana pool script  thx to Quarris!

--- kubejs/client_scripts/manapool.js
+++ kubejs/client_scripts/manapool.js
@@ -9,16 +9,35 @@ ClientEvents.paintScreen(event => {
     return
   }
 
-  var hitResult = event.mc.hitResult // Thing we are looking at
-  if (hitResult.type == 'BLOCK') {
-    var pos = hitResult.blockPos
-    var block = new BlockContainerJS(event.mc.level, pos); // KJS Block Wrapper
-    if (block.id == 'botania:mana_pool') {
-      if (event.mc.player.getMainHandItem().item == 'twig_wand') { // Wand of the Forest
-        var displayText = `${block.entityData["mana"]}`
-        var textWidth = event.font.width(displayText)
-        event.text(displayText, (event.width - textWidth) / 2, event.height / 2 + 50, -11549999, true)
-      }
-    }
+  // Make sure we are holding Wand of the Forest
+  if (event.mc.player.getMainHandItem().item != 'twig_wand' && event.mc.player.getOffHandItem().item != 'twig_wand') {
+    return
   }
+
+  var hitResult = event.mc.hitResult // Thing we are looking at
+  // Make sure we are looking at the block
+  if (hitResult.type != 'BLOCK') {
+    return
+  }
+
+  var block = new BlockContainerJS(event.mc.level, hitResult.blockPos); // KJS Block Wrapper
+  // Make sure our block has Mana
+  if (block != 'botania:mana_pool' &&
+    block != 'botania:diluted_pool' &&
+    block != 'botania:fabulous_pool') {
+    return
+  }
+
+  var dataText = `${block.entityData['mana']}`
+  // If the block entity has the 'manaCap' data, display it too
+  if (block.entityData['manaCap']) {
+    dataText += ` / ${block.entityData['manaCap']}`
+  }
+
+  var height = event.height / 2 + 51
+
+  // Render Block Data
+  var dataWidth = event.font.width(dataText) // Width of the data string
+  event.text('Mana', (event.width - event.font.width('Mana')) / 2, height, -11549999, true)
+  event.text(dataText, (event.width - dataWidth) / 2, height + 11, -11549999, true)
 })
