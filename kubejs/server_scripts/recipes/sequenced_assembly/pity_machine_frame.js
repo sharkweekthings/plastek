ecda4f51854d038dc67085fa7c079a1c83b25fb9
renik <mrushvg@gmail.com>
2023-10-07 00:25:43 -0700

    update pity machine frame recipe

--- kubejs/server_scripts/recipes/sequenced_assembly/pity_machine_frame.js
+++ kubejs/server_scripts/recipes/sequenced_assembly/pity_machine_frame.js
@@ -21,7 +21,7 @@ ServerEvents.recipes((event) => {
   event.custom({
     type: "create:sequenced_assembly",
      ingredient: {
-      item: "minecraft:stripped_oak_wood",},
+      item: "emendatusenigmatica:brass_block",},
      results: [{
       item: "industrialforegoing:machine_frame_pity",
       count: 1,},],
@@ -30,42 +30,42 @@ ServerEvents.recipes((event) => {
 {
     type: "create:deploying",
      ingredients: [{
-      item: "minecraft:stripped_oak_wood",},[{ // incomplete item
+      item: "emendatusenigmatica:brass_block",},[{ // incomplete item
       item: "emendatusenigmatica:iron_rod",},],], // item to deploy
      results: [{
-      item: "minecraft:stripped_oak_wood",},],
+      item: "emendatusenigmatica:brass_block",},],
 },
 {
     type: "create:deploying",
      ingredients: [{
-      item: "minecraft:stripped_oak_wood",},[{ // incomplete item
-      item: "emendatusenigmatica:iron_plate",},],], // item to deploy
+      item: "emendatusenigmatica:brass_block",},[{ // incomplete item
+      item: "emendatusenigmatica:steel_plate",},],], // item to deploy
      results: [{
-      item: "minecraft:stripped_oak_wood",},],
+      item: "emendatusenigmatica:brass_block",},],
 },
 {
     type: "create:deploying",
      ingredients: [{
-      item: "minecraft:stripped_oak_wood",},[{ // incomplete item
-      item: "emendatusenigmatica:iron_plate",},],], // item to deploy
+      item: "emendatusenigmatica:brass_block",},[{ // incomplete item
+      item: "emendatusenigmatica:steel_plate",},],], // item to deploy
      results: [{
-      item: "minecraft:stripped_oak_wood",},],
+      item: "emendatusenigmatica:brass_block",},],
 },
 {
     type: "create:filling",
      ingredients: [{
-      item: "minecraft:stripped_oak_wood",},{
+      item: "emendatusenigmatica:brass_block",},{
       fluid: "pneumaticcraft:plastic",
       amount: 300,},],
      results: [{
-      item: "minecraft:stripped_oak_wood",},],
+      item: "emendatusenigmatica:brass_block",},],
 },
 {
     type: "create:pressing",
      ingredients: [{
-      item: "minecraft:stripped_oak_wood",},],
+      item: "emendatusenigmatica:brass_block",},],
      results: [{
-      item: "minecraft:stripped_oak_wood",},],
+      item: "emendatusenigmatica:brass_block",},],
 },
 ],
     transitionalItem: {
