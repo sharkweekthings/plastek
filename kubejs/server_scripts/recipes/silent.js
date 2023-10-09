eb171bb43c1ae8526e8baa9460974e08fe111ab1
renik <mrushvg@gmail.com>
2023-10-08 20:42:34 -0700

    crystallizer to make arcmetal nugget, removing unpacking recipe

--- kubejs/server_scripts/recipes/silent.js
+++ kubejs/server_scripts/recipes/silent.js
@@ -48,6 +48,7 @@ ServerEvents.recipes(event => {
   event.remove({id: 'minecraft:arcmetal_ore_to_ingot_blasting'})
   event.remove({id: 'minecraft:raw_arcmetal_to_ingot'})
   event.remove({id: 'minecraft:arcmetal_ore_to_ingot'})
+  event.remove({id: 'minecraft:arcmetal_ingot_unpacking'})
 
 //
 })
